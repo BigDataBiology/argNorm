da179ffcbba276d71493de9987f02ae965526170
Luis Pedro Coelho <luis@luispedro.org>
2022-06-14 18:35:27 +0200

    TST RFCT Simpler tests with pytest infrastructure

--- tests/main.py
+++ tests/main.py
@@ -1,31 +1,11 @@
-import unittest
+import pytest
 import argNorm.normalizers as argnorm
 
 
-class TestARGSOAPNormalizer(unittest.TestCase):
+@pytest.mark.parametrize("is_hamronized", [True, False])
+@pytest.mark.parametrize("mode", ['reads', 'orfs'])
+def test_run(is_hamronized, mode):
+    example_dir = 'hamronized' if is_hamronized else 'raw'
 
-    def test_raw_reads_mode_input(self):
-        self.__test_run(is_hamronized=False, mode='reads')
-
-    def test_raw_orfs_mode_input(self):
-        self.__test_run(is_hamronized=False, mode='orfs')
-
-    def test_ham_reads_mode_input(self):
-        self.__test_run(is_hamronized=True, mode='reads')
-        
-    def test_ham_orfs_mode_input(self):
-        self.__test_run(is_hamronized=True, mode='orfs')
-
-    def __test_run(self, is_hamronized, mode):
-        if is_hamronized:
-            prefix = 'examples/hamronized/'
-        else:
-            prefix = 'examples/raw/'
-        try:
-            norm = argnorm.ARGSOAPNormalizer(is_hamronized=is_hamronized, mode=mode)
-            norm.run(input_file=prefix + f'args-oap.sarg.{mode}.tsv')
-            raised = False
-        except Exception as e:
-            print(str(e))
-            raised = True
-        self.assertEqual(raised, False)
+    norm = argnorm.ARGSOAPNormalizer(is_hamronized=is_hamronized, mode=mode)
+    norm.run(input_file=f'examples/{example_dir}/args-oap.sarg.{mode}.tsv')
