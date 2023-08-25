a5df660efeea3d21bf9e41cebf3a9b6fb929d567
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-08-25 18:48:44 +0530

    Used zip, instead of range(len()). Updated test cases and output to have tuples

--- tests/test_arg_category.py
+++ tests/test_arg_category.py
@@ -4,22 +4,26 @@ def test_get_immediate_drug_classes():
     test_cases = ['ARO:3003938', 'ARO:3000014']
 
     expected_output = [
-        [['ARO:3000008', 'penam'], ['ARO:0000032', 'cephalosporin'], ['ARO:0000044', 'cephamycin']],
-        [['ARO:3000008', 'penam'], ['ARO:0000004', 'monobactam'], ['ARO:3003706', 'penem'], ['ARO:0000032', 'cephalosporin'], ['ARO:3000637', 'ampicillin']]
+        [('ARO:3000008', 'penam'), ('ARO:0000032', 'cephalosporin'), ('ARO:0000044', 'cephamycin')],
+        [('ARO:3000008', 'penam'), ('ARO:0000004', 'monobactam'), ('ARO:3003706', 'penem'), ('ARO:0000032', 'cephalosporin'), ('ARO:3000637', 'ampicillin')]
     ]
 
-    for i in range(len(test_cases)):
-        assert sorted(drug_categorization.get_immediate_drug_classes(test_cases[i])) == sorted(expected_output[i])
+    zipped_test_cases_and_output = zip(test_cases, expected_output)
+
+    for i in zipped_test_cases_and_output:
+        assert sorted(drug_categorization.get_immediate_drug_classes(i[0])) == sorted(i[1])
 
 def test_get_drug_class_category():
     test_cases = [
-        [['ARO:0000004', 'monobactam']],
-        [['ARO:3000157', 'rifamycin antibiotic']],
-        [['ARO:0000030', 'tigecycline']],
-        [['ARO:0000036', 'ciprofloxacin']]
+        [('ARO:0000004', 'monobactam')],
+        [('ARO:3000157', 'rifamycin antibiotic')],
+        [('ARO:0000030', 'tigecycline')],
+        [('ARO:0000036', 'ciprofloxacin')]
     ]
 
     expected_output = [['beta-lactam antibiotic'], ['rifamycin antibiotic'], ['tetracycline antibiotic'], ['fluoroquinolone antibiotic']]
 
-    for i in range(len(test_cases)):
-        assert drug_categorization.get_drug_class_category(test_cases[i]) == expected_output[i]
+    zipped_test_cases_and_output = zip(test_cases, expected_output)
+
+    for i in zipped_test_cases_and_output:
+        assert drug_categorization.get_drug_class_category(i[0]) == i[1]
