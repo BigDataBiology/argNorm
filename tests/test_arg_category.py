5610fad1426c8fc4d4f7fd9723a45efd2834d144
Vedanth Ramji <vedanth.ramji@outlook.com>
2024-03-11 16:46:43 +1000

    Added new column headings. Index removed in output

--- tests/test_arg_category.py
+++ tests/test_arg_category.py
@@ -1,9 +1,9 @@
 from argnorm.drug_categorization import (
-    get_immediate_drug_classes,
-    get_drug_class_category,
+    get_inaffective_drugs,
+    get_inaffective_drug_classes
 )
 
-def test_get_immediate_drug_classes():
+def test_get_inaffective_drugs():
     test_cases = ["ARO:3003938", "ARO:3000014"]
 
     expected_output = [
@@ -24,10 +24,10 @@ def test_get_immediate_drug_classes():
     zipped_test_cases_and_output = zip(test_cases, expected_output)
 
     for i in zipped_test_cases_and_output:
-        assert sorted(get_immediate_drug_classes(i[0])) == sorted(i[1])
+        assert sorted(get_inaffective_drugs(i[0])) == sorted(i[1])
 
 
-def test_get_drug_class_category():
+def test_get_inaffective_drug_classes():
     test_cases = [
         [("ARO:0000004", "monobactam")],
         [("ARO:3000157", "rifamycin antibiotic")],
@@ -45,4 +45,4 @@ def test_get_drug_class_category():
     zipped_test_cases_and_output = zip(test_cases, expected_output)
 
     for i in zipped_test_cases_and_output:
-        assert get_drug_class_category(i[0]) == i[1]
+        assert get_inaffective_drug_classes(i[0]) == i[1]
