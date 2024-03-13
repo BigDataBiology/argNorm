bd58c1c2623a5c1fabaa1a08cfe9d83d5964da05
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2024-03-14 09:47:34 +1000

    Remove Python syntax in output

--- tests/test_arg_category.py
+++ tests/test_arg_category.py
@@ -5,43 +5,21 @@ from argnorm.drug_categorization import (
 
 def test_confers_resistance_to():
     test_cases = ["ARO:3003938", "ARO:3000014"]
-
+    
     expected_output = [
-        [
-            ("ARO:3000008", "penam"),
-            ("ARO:0000032", "cephalosporin"),
-            ("ARO:0000044", "cephamycin"),
-        ],
-        [
-            ("ARO:3000008", "penam"),
-            ("ARO:0000004", "monobactam"),
-            ("ARO:3003706", "penem"),
-            ("ARO:0000032", "cephalosporin"),
-            ("ARO:3000637", "ampicillin"),
-        ],
+        ["ARO:3000008", "ARO:0000032", "ARO:0000044"],
+        ["ARO:3000008", "ARO:0000004", "ARO:3003706", "ARO:0000032", "ARO:3000637"]
     ]
-
+    
     zipped_test_cases_and_output = zip(test_cases, expected_output)
 
     for i in zipped_test_cases_and_output:
         assert sorted(confers_resistance_to(i[0])) == sorted(i[1])
 
 
-def test_get_inaffective_drug_classes():
-    test_cases = [
-        [("ARO:0000004", "monobactam")],
-        [("ARO:3000157", "rifamycin antibiotic")],
-        [("ARO:0000030", "tigecycline")],
-        [("ARO:0000036", "ciprofloxacin")],
-    ]
-
-    expected_output = [
-        [("ARO:3000007", "beta-lactam antibiotic")],
-        [("ARO:3000157", "rifamycin antibiotic")],
-        [("ARO:3000050", "tetracycline antibiotic")],
-        [("ARO:0000001", "fluoroquinolone antibiotic")],
-    ]
-
+def test_drug_to_drug_classes():
+    test_cases = [["ARO:0000004"], ["ARO:3000157"], ["ARO:0000030"], ["ARO:0000036"]]
+    expected_output = [["ARO:3000007"], ["ARO:3000157"], ["ARO:3000050"], ["ARO:0000001"]]
     zipped_test_cases_and_output = zip(test_cases, expected_output)
 
     for i in zipped_test_cases_and_output:
