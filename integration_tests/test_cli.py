38539d618bd782ba1bd452201e8da596b21b76fc
Vedanth Ramji <vedanth.ramji@outlook.com>
2024-03-22 16:04:58 +1000

    simplified integration test code

--- integration_tests/test_cli.py
+++ integration_tests/test_cli.py
@@ -5,9 +5,14 @@ import os
 os.makedirs('integration_tests/outputs/raw', exist_ok=True)
 os.makedirs('integration_tests/outputs/hamronized', exist_ok=True)
 
-def perform_cli_test(tool, file, folder, db=None):
-    command = ['argnorm', tool, '-i', f'examples/{folder}/{file}', '-o', f'integration_tests/outputs/{folder}/{file}']
-    
+def test_cli(tool, file, folder, db=None):
+    command = [
+        'argnorm', 
+        tool, 
+        '-i', f'examples/{folder}/{file}', 
+        '-o', f'integration_tests/outputs/{folder}/{file}'
+    ]
+
     if folder == 'hamronized':
         command.append('--hamronized')
 
@@ -20,42 +25,15 @@ def perform_cli_test(tool, file, folder, db=None):
     golden_file = pd.read_csv(f'outputs/{folder}/{file}', sep='\t')
     pd.testing.assert_frame_equal(output, golden_file)
 
-def test_argsoap_cli(folder):
-    file = 'args-oap.sarg.reads.tsv'
-    perform_cli_test('argsoap', file, folder)
-
-test_argsoap_cli('hamronized')
-test_argsoap_cli('raw')
-
-def test_deeparg_cli(folder):
-    file = 'deeparg.deeparg.orfs.tsv'
-    perform_cli_test('deeparg', file, folder)
-
-test_deeparg_cli('hamronized')
-test_deeparg_cli('raw')
-
-def test_abricate_normalizer_cli(database, folder):
-    file = f'abricate.{database}.tsv'
-
-    if not (folder == 'raw' and db == 'resfinder'):
-        perform_cli_test('abricate', file, folder, db=database)
+for folder in ['hamronized', 'raw']:
+    test_cli('argsoap', 'args-oap.sarg.reads.tsv', folder)
+    test_cli('deeparg', 'deeparg.deeparg.orfs.tsv', folder)
+    test_cli('amrfinderplus', 'amrfinderplus.ncbi.orfs.tsv', folder)
+    test_cli('resfinder', 'resfinder.resfinder.reads.tsv', folder)
+    test_cli('resfinder', 'resfinder.resfinder.orfs.tsv', folder)
 
 for db in ['argannot', 'megares', 'ncbi', 'resfinder']:
-    test_abricate_normalizer_cli(db, 'hamronized')
-    test_abricate_normalizer_cli(db, 'raw')
-
-def test_resfinder_cli(folder, mode):
-    file = f'resfinder.resfinder.{mode}.tsv'
-    perform_cli_test('resfinder', file, folder)
-
-test_resfinder_cli('hamronized', 'reads')
-test_resfinder_cli('hamronized', 'orfs')
-test_resfinder_cli('raw', 'reads')
-test_resfinder_cli('raw', 'orfs')
-
-def test_amrfinderplus_cli(folder):
-    file = 'amrfinderplus.ncbi.orfs.tsv'
-    perform_cli_test('amrfinderplus', file, folder)
-
-test_amrfinderplus_cli('hamronized')
-test_amrfinderplus_cli('raw')
+    file = f'abricate.{db}.tsv'
+    test_cli('abricate', file, 'hamronized', db=db)
+    if db != 'resfinder':
+        test_cli('abricate', file, 'raw', db=db)
