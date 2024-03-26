f755fab2598075e73158d447fbbdcbca662db469
Vedanth Ramji <vedanth.ramji@outlook.com>
2024-03-26 11:04:37 +1000

    ENH Update drug categorization to use drug categories of superclasses.

--- argnorm/drug_categorization.py
+++ argnorm/drug_categorization.py
@@ -1,71 +1,65 @@
-# These functions are run in the run function of the BaseNormalizer.
-# After ARO numbers are obtained, these functions can be executed - hence should be independent of which db is used.
-
 import pronto
-from typing import List, Tuple
+from typing import List
 
 # Load the ARO ontology from internet
 ARO = pronto.Ontology.from_obo_library('aro.obo')
 
-def confers_resistance_to(aro_num: str) -> List[Tuple]:
+def confers_resistance_to(aro_num: str) -> List[str]:
     '''
-    Description: Gets the drugs/antibiotics to which a gene confers resistance to.
-    Only lists the drug class column in the CARD db.
+    Description: Returns a list of the drugs/antibiotics to which a gene confers resistance to.
 
     Parameters:
         aro_num (str): ARO number. Needs to be in the form 'ARO:number'.
 
     Returns:
-        drugs_list (list[tuple]):
-            A two-dimensional list where each inner list represents a drug.
-            Each inner list contains the ARO number and name of the drug in that order. [ARO:number, name].
+        drugs_list (list[str]):
+            A list with ARO number of the drugs/antibiotics to which the input gene confers resistance to.
     '''
 
-    # Some databases don't provide aro numbers as strings.
-    # Converting those aro numbers to pronto's desired format.
+    if aro_num not in ARO.terms():
+        return []
+    
     if type(aro_num) == float or type(aro_num) == int:
         aro_num = 'ARO:' + str(aro_num)
 
-    # If dealing with aro nans, the final drug class categorization will give [].
-    # Hence, immediate drug classes categorization also gives empty list.
-    if aro_num == 'ARO:nan':
-        return []
-
-    # Returning empty list if aro number not in ARO ontology.
-    if aro_num not in ARO.terms():
-        return []
-
+    drugs_list = []
+    found_drugs_list = False
     gene = ARO[aro_num]
 
-    confers_resistance_to_drug_class = any(r.name == 'confers_resistance_to_drug_class' for r in gene.relationships)
-    confers_resistance_to_antibiotic = any(r.name == 'confers_resistance_to_antibiotic' for r in gene.relationships)
+    while not found_drugs_list:
+        confers_resistance_to_drug_class = any(r.name == 'confers_resistance_to_drug_class' for r in gene.relationships)
+        confers_resistance_to_antibiotic = any(r.name == 'confers_resistance_to_antibiotic' for r in gene.relationships)
 
-    drugs_list = []
+        if confers_resistance_to_drug_class:
+            found_drugs_list = True
+            for drug in gene.relationships[ARO.get_relationship('confers_resistance_to_drug_class')]:
+                drugs_list.append(drug.id)
 
-    if confers_resistance_to_drug_class:
-        for drug in gene.relationships[ARO.get_relationship('confers_resistance_to_drug_class')]:
-            drugs_list.append(drug.id)
+        if confers_resistance_to_antibiotic:
+            found_drugs_list = True
+            for drug in gene.relationships[ARO.get_relationship('confers_resistance_to_antibiotic')]:
+                drugs_list.append(drug.id)
 
-    if confers_resistance_to_antibiotic:
-        for drug in gene.relationships[ARO.get_relationship('confers_resistance_to_antibiotic')]:
-            drugs_list.append(drug.id)
+        # ARO:1000001 is 'process or component of antibiotic biology or chemistry'
+        if gene.id == 'ARO:1000001':
+            break
 
+        gene = list(gene.superclasses(1))[1]
+    
     return sorted(drugs_list)
 
-def drugs_to_drug_classes(drugs_list: List[Tuple]) -> List[Tuple]:
+def drugs_to_drug_classes(drugs_list: List[str]) -> List[str]:
     '''
-    Description: Gives a list of categories of drug classes, e.g. cephem and penam are categorized as beta_lactam antibiotics.
+    Description: Returns a list of categories of drug classes, e.g. cephem and penam are categorized as beta_lactam antibiotics.
 
     Parameters:
-        drugs_list (list[tuple]):
-            A two-dimensional list where each inner list represents a drug.
-            Each inner list contains the ARO number and name of the drug in that order. [ARO:number, name].
-            Designed to use the return value of the function 'confers_resistance_to'.
+        drugs_list (list[str]):
+            A list with ARO number of the drugs/antibiotics to which a gene confers resistance to.
 
     Returns:
         drug_classes (list[str]):
-            A list containing the names of the drug classes of each drug given as input to the function.
-            Order of the names of the drug classes corresponds to the order in which the drugs were given
+            A list containing the ARO numbers of the drug classes of each drug given as input to the function.
+            Order of the ARO numbers of the drug classes corresponds to the order in which the drugs were given
             to the function in the drugs_list.
     '''
     drug_classes = []
--- tests/test_arg_category.py
+++ tests/test_arg_category.py
@@ -4,11 +4,16 @@ from argnorm.drug_categorization import (
 )
 
 def test_confers_resistance_to():
-    test_cases = ["ARO:3003938", "ARO:3000014"]
+    test_cases = [
+        "ARO:3003938", 
+        "ARO:3000014",
+        "ARO:3003725"
+    ]
     
     expected_output = [
         ["ARO:3000008", "ARO:0000032", "ARO:0000044"],
-        ["ARO:3000008", "ARO:0000004", "ARO:3003706", "ARO:0000032", "ARO:3000637"]
+        ["ARO:3000008", "ARO:0000004", "ARO:3003706", "ARO:0000032", "ARO:3000637"],
+        ["ARO:3000081"]
     ]
     
     zipped_test_cases_and_output = zip(test_cases, expected_output)
@@ -18,8 +23,20 @@ def test_confers_resistance_to():
 
 
 def test_drug_to_drug_classes():
-    test_cases = [["ARO:0000004"], ["ARO:3000157"], ["ARO:0000030"], ["ARO:0000036"]]
-    expected_output = [["ARO:3000007"], ["ARO:3000157"], ["ARO:3000050"], ["ARO:0000001"]]
+    test_cases = [
+        ["ARO:0000004"], 
+        ["ARO:3000157"], 
+        ["ARO:0000030"], 
+        ["ARO:0000036"],
+        ["ARO:3000081"]
+    ]
+    expected_output = [
+        ["ARO:3000007"], 
+        ["ARO:3000157"], 
+        ["ARO:3000050"], 
+        ["ARO:0000001"],
+        ["ARO:3000081"]
+    ]
     zipped_test_cases_and_output = zip(test_cases, expected_output)
 
     for i in zipped_test_cases_and_output:
