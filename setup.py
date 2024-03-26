cc1949c062146874afa21c116cf337cdf7c2334f
Vedanth Ramji <vedanth.ramji@outlook.com>
2024-03-26 13:55:46 +1000

    Added more python versions in setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Development Status :: 4 - Beta",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
