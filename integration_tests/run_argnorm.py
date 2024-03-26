58bbd654193b4109afa28a67b5b01fe1eba100f9
Luis Pedro Coelho <luis@luispedro.org>
2024-03-26 18:30:51 +1000

    TST Avoid pytest conventions if not pytest-compatible
    This way calling pytest at root directory just works

--- integration_tests/run_argnorm.py
+++ integration_tests/run_argnorm.py
@@ -5,11 +5,11 @@ import os
 os.makedirs('integration_tests/outputs/raw', exist_ok=True)
 os.makedirs('integration_tests/outputs/hamronized', exist_ok=True)
 
-def test_cli(tool, file, folder, db=None):
+def run_cli_test(tool, file, folder, db=None):
     command = [
-        'argnorm', 
-        tool, 
-        '-i', f'examples/{folder}/{file}', 
+        'argnorm',
+        tool,
+        '-i', f'examples/{folder}/{file}',
         '-o', f'integration_tests/outputs/{folder}/{file}'
     ]
 
@@ -26,14 +26,14 @@ def test_cli(tool, file, folder, db=None):
     pd.testing.assert_frame_equal(output, golden_file)
 
 for folder in ['hamronized', 'raw']:
-    test_cli('argsoap', 'args-oap.sarg.reads.tsv', folder)
-    test_cli('deeparg', 'deeparg.deeparg.orfs.tsv', folder)
-    test_cli('amrfinderplus', 'amrfinderplus.ncbi.orfs.tsv', folder)
-    test_cli('resfinder', 'resfinder.resfinder.reads.tsv', folder)
-    test_cli('resfinder', 'resfinder.resfinder.orfs.tsv', folder)
+    run_cli_test('argsoap', 'args-oap.sarg.reads.tsv', folder)
+    run_cli_test('deeparg', 'deeparg.deeparg.orfs.tsv', folder)
+    run_cli_test('amrfinderplus', 'amrfinderplus.ncbi.orfs.tsv', folder)
+    run_cli_test('resfinder', 'resfinder.resfinder.reads.tsv', folder)
+    run_cli_test('resfinder', 'resfinder.resfinder.orfs.tsv', folder)
 
 for db in ['argannot', 'megares', 'ncbi', 'resfinder']:
     file = f'abricate.{db}.tsv'
-    test_cli('abricate', file, 'hamronized', db=db)
+    run_cli_test('abricate', file, 'hamronized', db=db)
     if db != 'resfinder':
-        test_cli('abricate', file, 'raw', db=db)
+        run_cli_test('abricate', file, 'raw', db=db)
