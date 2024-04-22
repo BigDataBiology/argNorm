3e68cb8bf3e8815897545b01428c8ecdf17fddf3
Vedanth <vedanth.ramji@outlook.com>
2024-04-22 14:20:05 +0530

    ENH: Use amino acid version of resfinder database
    - Resfinder is comprised of coding sequences. The data wasn't being handled properly before as contig mode was used when passing coding sequences to RGI. Now, the amino acid versions of Resfinder is used with protein mode when running the database in RGI.
    - Resfinder AA file is generated using biopython from nucleotide file (AA file not found online).
    - 9 RC genes were found (previously 4 were found). All were manually curated as RC versions couldn't be translated properly into AA sequences.
    - Documentation updated in changelog to reflect AA version being used for Resfinder and gene cluster handling

--- db_harmonisation/get_mapping_table.py
+++ db_harmonisation/get_mapping_table.py
@@ -13,7 +13,7 @@ def check_file(path):
         return path
     else:
         raise argparse.ArgumentTypeError(f"{path} can't be read")
-    
+
 def get_aro_for_hits(fa, rgi_output, database):
     database_entries = []
     for record in SeqIO.parse(str(fa), 'fasta'):
@@ -26,7 +26,7 @@ def get_aro_for_hits(fa, rgi_output, database):
     rgi_hits = pd.read_csv(rgi_output, sep='\t')
 
     if database == 'resfinder':
-        rgi_hits['Original ID'] = rgi_hits['Contig'].apply(lambda x: "_".join(x.split('_')[:-1]))
+        rgi_hits['Original ID'] = rgi_hits['ORF_ID']
     elif database == 'ncbi':
         rgi_hits['Original ID'] = rgi_hits['ORF_ID']
     elif database == 'sarg':
