de775cdcf7a79e3f8a2dfdd38b771a003c48b753
Vedanth <vedanth.ramji@outlook.com>
2024-04-25 15:56:43 +0530

    RFCT removed special casing for resfinder manual curation
    - Note: code to integrate manual curation now removes duplicate ARO mappings. This has corrected a MEGARes annotation (GMGC10.027_903_362.EMRE) which had a one to many ARO mapping. Better manual curation for MEGARes will be present in the version after v0.3.0 when MEGARes will be investigated to check for CDSs, gene clusters and RC genes.

--- argnorm/lib.py
+++ argnorm/lib.py
@@ -17,24 +17,18 @@ def is_number(num):
     return True
 
 def get_aro_mapping_table(database):
-    df = pd.read_csv(os.path.join(_ROOT, 'data', f'{database}_ARO_mapping.tsv'), sep='\t')
+    aro_mapping_table = pd.read_csv(os.path.join(_ROOT, 'data', f'{database}_ARO_mapping.tsv'), sep='\t')
 
     manual_curation = pd.read_csv(os.path.join(_ROOT, 'data/manual_curation', f'{database}_curation.tsv'), sep='\t')
-    manual_curation['Database'] = df['Database']
-    aro_mapping_table = df
+    manual_curation['Database'] = aro_mapping_table['Database']
 
-    if database != 'resfinder':
-        aro_mapping_table = pd.concat([df, manual_curation])
-    else:
-        # Handle gene clusters and reverse complements
-        aro_mapping_table = aro_mapping_table.drop_duplicates(subset=['Original ID'], ignore_index=True).set_index('Original ID')
-
-        for i in manual_curation['Original ID']:
-            if i in aro_mapping_table.index:
-                aro_mapping_table.loc[i, 'ARO'] = manual_curation.set_index('Original ID').loc[i, 'ARO']
-                aro_mapping_table.loc[i, 'Gene Name in CARD'] = manual_curation.set_index('Original ID').loc[i, 'Gene Name in CARD']
-            else:
-                aro_mapping_table.loc[i] = manual_curation.set_index('Original ID').loc[i]
+    aro_mapping_table = aro_mapping_table.drop_duplicates(subset=['Original ID'], ignore_index=True).set_index('Original ID')
+    for i in manual_curation['Original ID']:
+        if i in aro_mapping_table.index:
+            aro_mapping_table.loc[i, 'ARO'] = manual_curation.set_index('Original ID').loc[i, 'ARO']
+            aro_mapping_table.loc[i, 'Gene Name in CARD'] = manual_curation.set_index('Original ID').loc[i, 'Gene Name in CARD']
+        else:
+            aro_mapping_table.loc[i] = manual_curation.set_index('Original ID').loc[i]
 
     aro_mapping_table[TARGET_ARO_COL] = aro_mapping_table[TARGET_ARO_COL].map(lambda a: f'ARO:{int(a)}' if is_number(a) else a)
     return aro_mapping_table.reset_index()
