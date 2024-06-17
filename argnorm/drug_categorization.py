7d2224cb6349ecd565e29646ee7a6fa34435e9e5
Vedanth <vedanth.ramji@outlook.com>
2024-06-17 19:46:43 +0530

    ENH Update drug categorization logic
    - Navigate up entire ARO tree for genes so drugs of gene families can be included
    - Antibiotic mixture ARO mapping is excluded except cases of antibiotic + adjuvant mixtures
    - Use 'has_part' relationship to handle antibiotic mixtures and give drug class rather than 'antibiotic mixture'

--- argnorm/drug_categorization.py
+++ argnorm/drug_categorization.py
@@ -2,8 +2,30 @@ from typing import List
 from . import lib
 
 ARO = lib.get_aro_ontology()
+
 confers_resistance_to_drug_class_rel = ARO.get_relationship('confers_resistance_to_drug_class')
 confers_resistance_to_antibiotic_rel = ARO.get_relationship('confers_resistance_to_antibiotic')
+has_part_rel = ARO.get_relationship('has_part')
+is_small_inhibitor_molecule_rel = ARO.get_relationship('is_small_molecule_inhibitor')
+
+def navigate_superclasses(super_classes_list: List[str]) -> List[str]:
+    """
+    - Helper function to traverse up and record superclasses in ARO
+    - Traverses up ARO until 'antibiotic molecule' class reached and 'antibiotic mixture' class not reached
+    - antibiotic molecule -> ARO:1000003
+    - antibiotic mixture -> ARO:3000707
+    """
+    output = []
+
+    for super_class in super_classes_list:
+        super_class_classes = list(super_class.superclasses(1))
+        antibiotic_molecule_node = [ARO['ARO:1000003']]
+
+        # checking if immediate child of 'antibiotic molecule' is reached & it is not 'antibiotic mixture'
+        if super_class_classes[1:] == antibiotic_molecule_node and super_class.id != 'ARO:3000707':
+            output.append(super_class.id)
+
+    return output
 
 def confers_resistance_to(aro_num: str) -> List[str]:
     '''
@@ -16,20 +38,29 @@ def confers_resistance_to(aro_num: str) -> List[str]:
         target (list[str]):
             A list with ARO number of the drugs/antibiotics to which the input gene confers resistance to.
     '''
-
+    antibiotic_molecule_node = [ARO['ARO:1000003'], ARO['ARO:1000001']]
+    # some gene superclasses can map to drugs which are immediate children of 'antibiotic molecule'
+    # only use these if no other drugs can be found, as this information will be present in
+    # drugs to drug classes
+    backup_drugs = []
     target = set()
-
     for term in ARO[aro_num].superclasses():
         for drug in term.relationships.get(confers_resistance_to_drug_class_rel, []):
-            target.add(drug.id)
+            if list(ARO[drug.id].superclasses())[1:] == antibiotic_molecule_node:
+                backup_drugs.append(drug.id)
+            else:
+                target.add(drug.id)
 
         for drug in term.relationships.get(confers_resistance_to_antibiotic_rel, []):
-            target.add(drug.id)
+            if list(ARO[drug.id].superclasses())[1:] == antibiotic_molecule_node:
+                backup_drugs.append(drug.id)
+            else:
+                target.add(drug.id)
 
-        if target:
-            break
+    if target == set():
+        target.update(backup_drugs)
 
-    return sorted(target)
+    return sorted(list(target))
 
 def drugs_to_drug_classes(drugs_list: List[str]) -> List[str]:
     '''
@@ -50,11 +81,21 @@ def drugs_to_drug_classes(drugs_list: List[str]) -> List[str]:
     for drug in drugs_list:
         drug_instance = ARO[drug]
         drug_instance_superclasses = list(drug_instance.superclasses())
-        superclasses_len = len(drug_instance_superclasses)
+        drug_classes += navigate_superclasses(drug_instance_superclasses)
 
-        if superclasses_len >= 3:
-            drug_classes.append(drug_instance_superclasses[superclasses_len - 3].id)
-        else:
-            drug_classes.append(drug_instance_superclasses[0].id)
+        has_part_nodes = drug_instance.relationships.get(has_part_rel, [])
+        if has_part_nodes:
+            for hast_part_node in has_part_nodes:
+                hast_part_node_superclasses = list(hast_part_node.superclasses())[1:]
+
+                for super_class in hast_part_node_superclasses:
+                    super_class_categories = list(super_class.superclasses())
+                    drug_classes += navigate_superclasses(super_class_categories)
+
+                if hast_part_node.relationships.get(is_small_inhibitor_molecule_rel, []):
+                    drug_classes.append('ARO:3000707')
+
+        if drug_classes == []:
+            drug_classes.append(drug_instance.id)
 
     return sorted(drug_classes)
