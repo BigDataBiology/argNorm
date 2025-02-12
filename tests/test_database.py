33f4ceaa35d85ad3964a648e4f09c017202097b4
Vedanth <vedanth.ramji@outlook.com>
2025-02-12 15:23:17 +0530

    ENH: check RGI mapping accuracy and manually curate incorrect hits
    All RGI hits are checked for mapping accuracy using `check_mapping_accuracy.py`. This is done by comparing the drug categorization of the ARO assigned to the ARG with the drug categorization assigned to the ARG by its original database. A list of ARGs with mismatched mappings are included in `manual_curation/`. Mismatched hits are manually curated to correct ARGs and manual curation files are updated. Some mismatched hits are marked `correct` or `incorrect`. `correct` hits have a drug category mismatch but have an ARO mapping that's determined to be correct. `incorrect` hits have a drug category mismatch and a manually curated ARO can't be found for them. Both `correct` and `incorrect` hits should not be included in the final manual curation. For megares, a file called `megares_meta_biocide_and_virulence_genes.tsv` is also created with metal, biocide, and virulence genes. These should not be mapped to any ARO terms and can be manually curated to no ARO. Metal, biocide, and virulence genes are directly added to manual curation files for other databases. Groot is derived from resfinder and argannot.
    
    A list of all mismatched hits, with their manual curation can be found in `db_harmonisation/all_mismatched_hits.tsv`

--- tests/test_database.py
+++ tests/test_database.py
@@ -8,15 +8,25 @@ ARO = lib.get_aro_ontology()
 def test_database(database):
     determinant_of_ab_resistance = ARO['ARO:3000000']
     antibiotic_molecule = ARO['ARO:1000003']
+    process_or_component_of_antibiotic_biology_or_chemistry = ARO['ARO:1000001']
     assert determinant_of_ab_resistance.name == 'determinant of antibiotic resistance'
     assert antibiotic_molecule.name == 'antibiotic molecule'
+    assert process_or_component_of_antibiotic_biology_or_chemistry.name == "process or component of antibiotic biology or chemistry"
 
     db = lib.get_aro_mapping_table(database)
     assert len(db.index) == len(set(db.index)), f'Duplicate gene names in {database}'
 
     for ar in set(db['ARO'].dropna()):
         assert ar in ARO, f'ARO not found in the ontology: {ar}'
-        assert determinant_of_ab_resistance in ARO[ar].superclasses(), f'ARO not a determinant of antibiotic resistance: {ar}'
+
+        try:
+            assert determinant_of_ab_resistance in ARO[ar].superclasses()
+        except:
+            try:
+                assert process_or_component_of_antibiotic_biology_or_chemistry in ARO[ar].superclasses()
+            except:
+                assert False, f'ARO not a determinant of antibiotic resistance nor process/component of ab resistance: {ar}'
+
         for d in drug_categorization.confers_resistance_to(ar):
             assert d in ARO, f'ARO not found in the ontology: {d}'
             assert antibiotic_molecule in ARO[d].superclasses(), f'ARO not an antibiotic molecule: {d}'
