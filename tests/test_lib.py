20e72c4bad957fb5ace678411a05a378599e5431
Vedanth <vedanth.ramji@outlook.com>
2025-06-24 07:09:12 +0530

    TST add test to make sure hamronized outputs and raw outputs give same AROs

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -9,7 +9,7 @@ def test_map_to_aro():
     test_cases = [
         ["(AGly)AAC(6')-Isa:NG_047311:101-574:474", 'argannot'],
         ["MEG_21|Drugs|Aminoglycosides|Aminoglycoside_N-acetyltransferases|AAC3", 'megares'],
-        ["1028085756|WP_063844287.1|1|1|cpt|cpt|phosphotransferase|2|CHLORAMPHENICOL|PHENICOL|chloramphenicol_phosphotransferase_CPT", 'ncbi'],
+        ["1028085756|WP_063844287.1|NG_047672.1|1|1|cpt|cpt|phosphotransferase|2|CHLORAMPHENICOL|PHENICOL|chloramphenicol_phosphotransferase_CPT", 'ncbi'],
         ["gb|AAG57600.1|ARO:3000318|mphB", "sarg"],
         ["(Phe)cpt_strepv:U09991:AAB36569:1412-1948:537", "argannot"],
         ["MEG_4060|Metals|Multi-metal_resistance|Multi-metal_resistance_protein|MREA", "megares"],
